3d7c31fb54a7bb96053430f7ed8144b217a02db2
Raul Metsma <raul@metsma.ee>
2018-05-29 14:37:41 +0300

    Update texts (#62)
    TERA-249
    
    Signed-off-by: Raul Metsma <raul@metsma.ee>

--- translations/ru.ts
+++ translations/ru.ts
@@ -12,10 +12,10 @@
     <message>
         <location filename="../common/AboutDialog.ui" line="62"/>
         <source>&lt;p&gt;Estonian ID-software is released by Information Systems&apos;s Auhtority&lt;br /&gt;
-In case of questions please conctact our support via &lt;a href=&quot;https://www.id.ee&quot;&gt;https://www.id.ee&lt;/a&gt; or by phone (+372) 666 8888.&lt;/p&gt;
+In case of questions please conctact our support via &lt;a href=&quot;https://www.id.ee&quot;&gt;https://www.id.ee&lt;/a&gt;.&lt;/p&gt;
 Additional licenses and components</source>
         <translation>&lt;p&gt;Разработчиком программного обеспечения ID-карты является Riigi Infosüsteemi Amet.&lt;br /&gt;
-Если у вас возникнут вопросы, пожалуйста, обратитесь на &lt;a href=&quot;https://www.id.ee&quot;&gt;https://www.id.ee&lt;/a&gt; или позвоните по телефону (+372) 666 8888.&lt;/p&gt;
+Если у вас возникнут вопросы, пожалуйста, обратитесь на &lt;a href=&quot;https://www.id.ee&quot;&gt;https://www.id.ee&lt;/a&gt;s.&lt;/p&gt;
 Дополнительные лицензии и компоненты</translation>
     </message>
     <message>
